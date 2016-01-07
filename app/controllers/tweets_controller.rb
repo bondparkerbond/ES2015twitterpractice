d18c78cf55e6871fa3f84787a360ae8c97e2c3ee
bondparkerbond <parkerbond@gmail.com>
2016-01-07 13:57:39 -0700

    start of backend and raw SQL search

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -9,6 +9,17 @@ class TweetsController < ApplicationController
     render 'tweet'
   end
 
+  def search
+    search_term = params[:search_term]
+    if search_term.split('').first == '@'
+      user = User.where('lower(handle) LIKE ?', "%#{search_term.downcase.split('@').last}%")
+      @tweets = Tweet.where('lower(body) LIKE ?', "%#{search_term.downcase}%")
+    else
+      @tweets = Tweet.where('lower(body) LIKE ?', "%#{search_term.downcase}%")
+    end
+    render 'index'
+  end
+
   private
 
   def tweet_params
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   
   root 'dashboard#index'
   resources :tweets
+  get 'tweet_search', to: 'tweets#search'
   devise_for :users, controllers: { registrations: 'registrations' }
   
 end
